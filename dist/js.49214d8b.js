c1fe58d832900c30662603198cad62eb777dbaf8
ceboulba <antoine@archipicture.fr>
2018-12-13 11:01:22 +0100

    000

--- dist/js.49214d8b.js
+++ dist/js.49214d8b.js
@@ -145,11 +145,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-<<<<<<< HEAD
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '57279' + '/');
-=======
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '52305' + '/');
->>>>>>> parent of 56c80bc... 456
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '57738' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
