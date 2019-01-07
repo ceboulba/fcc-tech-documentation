27231f1735199adb9359d54dc226880c05c3c0a5
ceboulba <antoine@archipicture.fr>
2019-01-07 09:45:28 +0100

    fromDesktop

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded')
 
-  const menu = document.querySelector('.menu')
+  // const menu = document.querySelector('.menu')
 
-  const calcDistance = () => {
-    const dist = menu.getBoundingClientRect();
-    return dist
-  }
+  // const calcDistance = () => {
+  //   const dist = menu.getBoundingClientRect();
+  //   return dist
+  // }
 
-  let toTop = calcDistance().y
-  let menuWidth = calcDistance().width
+  // let toTop = calcDistance().y
+  // let menuWidth = calcDistance().width
 
-  const toFixed = () => {
-    console.log('toTop=>',window.scrollY)
-    if(window.scrollY > toTop) {
-      menu.classList.add('fixed')
-      menu.style.width = `${menuWidth}px` ;
-    } else{
-      menu.classList.remove('fixed')
-    }
-    
-  }
+  // const toFixed = () => {
+  //   console.log('toTop=>',window.scrollY)
+  //   if(window.scrollY > toTop) {
+  //     menu.classList.add('fixed')
+  //     menu.style.width = `${menuWidth}px` ;
+  //   } else{
+  //     menu.classList.remove('fixed')
+  //   }
 
-  window.addEventListener('scroll', toFixed)
-  window.addEventListener('resize',()=>{
-    toTop = calcDistance().y
-    menuWidth = calcDistance().width
-    toFixed
-  })
+  // }
 
+  // window.addEventListener('scroll', toFixed)
+  // window.addEventListener('resize',()=>{
+  //   toTop = calcDistance().y
+  //   menuWidth = calcDistance().width
+  //   toFixed
+  // })
 })
